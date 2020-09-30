f4d054721ce7f717abab2c37b485e52a41f9ebae
Ashutoshh <asashuthegreat4@gmail.com>
2020-09-30 23:37:42 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM php:7-apache
+MAINTAINER  ashutoshh
+WORKDIR  /var/www/html
+ADD index.php   .
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<h1> Hello Docker </h1>
+
+	<?php 
+		echo phpinfo();
+	?>
+
+</body>
+</html>
